dc5b51b30c04d98da738968b279335ed3d3e22e2
Parapoy <145159048+Parapoy@users.noreply.github.com>
2023-12-27 00:05:20 +0100

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,16 +1,25 @@
-function getShippingCost(country){
+function getShippingCost(country) {
+  let price;
+
   switch (country){
-    case "China": 100
-    case "Chile": 250
-    case "Australia": 170 
-    case "Jamaica": 120 
+    case "China":
+      price = 100;
+    break;
+    case "Chile":
+      price = 250;
+    break;
+    case "Australia":
+      price = 170 ;
+    break;
+    case "Jamaica":
+      price = 120 ;
+    break;
   
   default:
-    console.log("Sorry, there is no delivery to your country")
-    return
+   return "Sorry, there is no delivery to your country";
   }
   
-  
+  return `Shipping to ${country} will cost ${price} credits`;
   }
   
   console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
@@ -19,3 +28,7 @@ function getShippingCost(country){
   console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
   console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
   console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
+  
+
+     
+    
